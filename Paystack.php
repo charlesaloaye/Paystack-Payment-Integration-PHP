66abb58979a6a4ca13ae87670e499057cd3ae5f5
Sedenu <aloayecharles@gmail.com>
2024-02-25 15:52:14 +0100

    Create Paystack.php

---
+++ Paystack.php
@@ -0,0 +1,41 @@
+<?php
+// url to go to after payment
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://api.paystack.co/transaction/initialize",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => json_encode([
+        'amount' => '',
+        'email' => '',
+        'phone' => '',
+        'currency' => '',
+        'callback_url' => ''
+    ]),
+    CURLOPT_HTTPHEADER => [
+        "authorization: Bearer " . PAYSTACK_API_KEY,
+        //api key
+        "content-type: application/json",
+        "cache-control: no-cache"
+    ],
+)
+);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+if ($err) {
+    // there was an error contacting the Paystack API
+    die('Curl returned error: ' . $err);
+}
+
+$tranx = json_decode($response, true);
+
+if (!$tranx['status']) {
+    // there was an error from the API
+    print_r('API returned error: ' . $tranx['message']);
+}
+// comment out this line if you want to redirect the user to the payment page
+print_r($tranx);
+// redirect to page so User can pay
+// uncomment this line to allow the user redirect to the payment page
+header('Location: ' . $tranx['data']['authorization_url']);
